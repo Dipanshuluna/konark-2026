441de5b72afceabb98fdbda07ab191663e27273d
Dipanshuluna <159991906+Dipanshuluna@users.noreply.github.com>
2026-01-20 11:15:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/konark/",
+  plugins: [react()],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
