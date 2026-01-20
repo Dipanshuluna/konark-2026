4c4a062251378ab6b222dddbb97f167726d1a57d
Dipanshuluna <159991906+Dipanshuluna@users.noreply.github.com>
2026-01-20 11:19:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: "/konark-2026/",
 
-  plugins: [react()],
-});
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/konark/",   // <-- ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
@@ -17,7 +13,12 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
